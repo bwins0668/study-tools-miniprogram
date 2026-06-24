70df776f06b690aa5d2af9a4667908429415ab51
lvguanbing <lvguanbing@gmail.com>
2026-06-24 18:09:18 +0900

    feat: add local deck manifests to every flashcard data subpackage
    Each data subpackage (quiz-itpass-1/2/3/4/5, quiz-sg-1/2) now has
    a local deck-manifest.js that wraps existing meta.js with a
    getDeckInfo(deckId) lookup. deckId format: 'course/yearId' (e.g.,
    'itpass/01_aki'). This enables the flashcard-player to resolve
    deck metadata within its own subpackage without cross-package
    requires or globalData cache.

---
+++ packages/quiz-itpass-4/data/deck-manifest.js
@@ -0,0 +1,32 @@
+'use strict';
+var meta = require('./meta');
+var DECK_MAP = {};
+for (var i = 0; i < meta.years.length; i++) {
+  var y = meta.years[i];
+  var deckId = y.exam + '/' + y.yearId;
+  DECK_MAP[deckId] = {
+    deckId: deckId,
+    course: y.exam,
+    yearId: y.yearId,
+    yearLabel: y.label,
+    expectedCount: y.count,
+    sourceType: 'past_exam_japanese'
+  };
+}
+function getDeckInfo(deckId) {
+  return DECK_MAP[deckId] || null;
+}
+function getAllLocalDecks() {
+  var result = [];
+  for (var key in DECK_MAP) {
+    if (Object.prototype.hasOwnProperty.call(DECK_MAP, key)) {
+      result.push(DECK_MAP[key]);
+    }
+  }
+  return result;
+}
+module.exports = {
+  getDeckInfo: getDeckInfo,
+  getAllLocalDecks: getAllLocalDecks,
+  meta: meta
+};
